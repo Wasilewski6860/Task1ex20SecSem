54b4a2fe8b7f42332128c044a409d51e4cf75669
Wasilewski6860 <trollivanovich6860@gmail.com>
2022-02-13 22:43:57 +0300

    Работает масштабирование относительно стороны1.

--- src/ru/vsu/baryshev/Main.java
+++ src/ru/vsu/baryshev/Main.java
@@ -1,15 +1,105 @@
 package ru.vsu.baryshev;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
 
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(new Rectangle.Point(0,20),new Rectangle.Point(40,0),new Rectangle.Point(80,70),new Rectangle.Point(40,90));
-	Rectangle.drawing(rectangle);
-        System.out.println("S= " + Rectangle.searchOfS(rectangle));
-        System.out.println("P= " + Rectangle.searchForP(rectangle));
 
-        Rectangle circedRectangle = Rectangle.createACircumscribedRectangle(rectangle);
-        Rectangle.drawing(circedRectangle);
+        JFrame testFrame = new JFrame();
+        testFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        final LinesComponent comp = new LinesComponent();
+        comp.setPreferredSize(new Dimension(400, 400));
+        testFrame.getContentPane().add(comp, BorderLayout.CENTER);
+        JPanel buttonsPanel = new JPanel();
+        JPanel textPanel = new JPanel();
+        JButton drawButton = new JButton("Draw");
+        JButton ShareButton = new JButton("Share");
+        JButton clearButton = new JButton("Clear");
+        JButton circumscribed = new JButton("Circumscribed");
+        JTextArea textField = new JTextArea("You Params");
+        textField.setVisible(true);
+        textField.setBackground(Color.WHITE);
+        textField.setSize(100,10);
+        buttonsPanel.add(drawButton);
+        buttonsPanel.add(ShareButton);
+        buttonsPanel.add(clearButton);
+        buttonsPanel.add(circumscribed);
+        buttonsPanel.add(textField);
+        testFrame.getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+        testFrame.getContentPane().add(buttonsPanel, BorderLayout.NORTH);
+
+        final Rectangle[] rectangle = {new Rectangle(new Rectangle.Point(20, 30), new Rectangle.Point(40, 10), new Rectangle.Point(80, 50), new Rectangle.Point(60, 70))};
+        //40,0 140,80 100,130 0,50
+        //100,100 200,100 200,200 100,200
+        //-100,-100 100,-100 100,100 -100,100
+        drawButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                String[] coordinates = textField.getText().split(";");
+//                Rectangle.Point p1 = new Rectangle.Point(Integer.parseInt(coordinates[0]),Integer.parseInt(coordinates[1]));
+//                Rectangle.Point p2 = new Rectangle.Point(Integer.parseInt(coordinates[2]),Integer.parseInt(coordinates[3]));
+//                Rectangle.Point p3 = new Rectangle.Point(Integer.parseInt(coordinates[4]),Integer.parseInt(coordinates[5]));
+//                Rectangle.Point p4 = new Rectangle.Point(Integer.parseInt(coordinates[6]),Integer.parseInt(coordinates[7]));
+//                Rectangle rec = new Rectangle(p1,p2,p3,p4);
+
+                comp.addLine(rectangle[0].p1.x, rectangle[0].p1.y, rectangle[0].p2.x, rectangle[0].p2.y, Color.BLACK);
+                comp.addLine(rectangle[0].p2.x, rectangle[0].p2.y, rectangle[0].p3.x, rectangle[0].p3.y, Color.BLACK);
+                comp.addLine(rectangle[0].p3.x, rectangle[0].p3.y, rectangle[0].p4.x, rectangle[0].p4.y, Color.BLACK);
+                comp.addLine(rectangle[0].p4.x, rectangle[0].p4.y, rectangle[0].p1.x, rectangle[0].p1.y, Color.BLACK);
+
+                textField.setText("S= " + Rectangle.searchOfS(rectangle[0])+ "P= " + Rectangle.searchForP(rectangle[0]));
+
+
+//                comp.addLine(p1.x, p1.y, p2.x, p2.y, Color.BLACK);
+//                comp.addLine(p2.x, p2.y, p3.x, p3.y, Color.BLACK);
+//                comp.addLine(p3.x, p3.y, p4.x, p4.y, Color.BLACK);
+//                comp.addLine(p4.x, p4.y, p1.x, p1.y, Color.BLACK);
+            }
+        });
+
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comp.clearLines();
+            }
+        });
+
+        circumscribed.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Rectangle circedRectangle = Rectangle.createACircumscribedRectangle(rectangle[0]);
+                comp.addLine(circedRectangle.p1.x, circedRectangle.p1.y, circedRectangle.p2.x, circedRectangle.p2.y, Color.BLACK);
+                comp.addLine(circedRectangle.p2.x, circedRectangle.p2.y, circedRectangle.p3.x, circedRectangle.p3.y, Color.BLACK);
+                comp.addLine(circedRectangle.p3.x, circedRectangle.p3.y, circedRectangle.p4.x, circedRectangle.p4.y, Color.BLACK);
+                comp.addLine(circedRectangle.p4.x, circedRectangle.p4.y, circedRectangle.p1.x, circedRectangle.p1.y, Color.BLACK);
+            }
+        });
+
+        ShareButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               rectangle[0] = Rectangle.moving(rectangle[0], Rectangle.typeOfHolding.SIDE, Rectangle.numberOFSelected.FIRST,2);
+                comp.addLine(rectangle[0].p1.x, rectangle[0].p1.y, rectangle[0].p2.x, rectangle[0].p2.y, Color.BLACK);
+                comp.addLine(rectangle[0].p2.x, rectangle[0].p2.y, rectangle[0].p3.x, rectangle[0].p3.y, Color.BLACK);
+                comp.addLine(rectangle[0].p3.x, rectangle[0].p3.y, rectangle[0].p4.x, rectangle[0].p4.y, Color.BLACK);
+                comp.addLine(rectangle[0].p4.x, rectangle[0].p4.y, rectangle[0].p1.x, rectangle[0].p1.y, Color.BLACK);
+                System.out.println("Shared");
+
+            }
+        });
+
+        testFrame.pack();
+        testFrame.setVisible(true);
+
+
+        System.out.println("S= " + Rectangle.searchOfS(rectangle[0]));
+        System.out.println("P= " + Rectangle.searchForP(rectangle[0]));
+
+
 
 
     }
--- src/ru/vsu/baryshev/Rectangle.java
+++ src/ru/vsu/baryshev/Rectangle.java
@@ -1,7 +1,9 @@
 package ru.vsu.baryshev;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import org.w3c.dom.css.Rect;
 
+import javax.sound.midi.Soundbank;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
@@ -9,10 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Rectangle {
+ class Rectangle {
 
     static final class Point {
-        public int x;
+        protected int x;
         public int y;
 
         public Point(int x, int y) {
@@ -26,8 +28,16 @@ public class Rectangle {
         }
 
     }
-
-
+    enum typeOfHolding{
+        POINT,
+        SIDE
+    }
+    enum numberOFSelected{
+        FIRST,
+        SECOND,
+        THIRD,
+        FORTH
+    }
 
     public Point p1;
     public Point p2;
@@ -35,43 +45,41 @@ public class Rectangle {
     public Point p4;
 
     public Rectangle(Point p1,Point p2,Point p3,Point p4){
-        this.p1=p1;
-        this.p2=p2;
-        this.p3=p3;
-        this.p4=p4;
+
+        if(( (p2.x-p1.x)*(p3.x- p2.x) + (p2.y-p1.y)*(p3.y- p2.y) == 0) &&( (p3.x-p2.x)*(p4.x- p3.x) + (p3.y-p2.y)*(p4.y- p3.y) == 0) &&( (p4.x-p3.x)*(p1.x- p4.x) + (p4.y-p3.y)*(p1.y- p4.y) == 0)  ) {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+            this.p4 = p4;
+        } else {
+            this.p1 = null;
+            this.p2 = null;
+            this.p3 = null;
+            this.p4 = null;
+            System.out.println("Не прямоугольник");
+        }
     }
 
 
-    public static void drawing(Rectangle rec){
-        Runnable r = new Runnable() {
-            public void run() {
-                ru.vsu.baryshev.LineComponent lineComponent = new ru.vsu.baryshev.LineComponent(400,400);
-
-                    lineComponent.addLine(rec.p1.x,rec.p1.y,rec.p2.x,rec.p2.y);
-                    lineComponent.addLine(rec.p2.x,rec.p2.y,rec.p3.x,rec.p3.y);
-                    lineComponent.addLine(rec.p3.x,rec.p3.y,rec.p4.x,rec.p4.y);
-                    lineComponent.addLine(rec.p4.x,rec.p4.y,rec.p1.x,rec.p1.y);
-
-                JOptionPane.showMessageDialog(null, lineComponent);
-            }
-        };
-        SwingUtilities.invokeLater(r);
-    }
 
     public static double searchOfS(Rectangle rec){
+
         double m = 0.5*(rec.p1.x*rec.p2.y + rec.p2.x*rec.p3.y + rec.p3.x*rec.p4.y + rec.p4.x*rec.p1.y - rec.p2.x*rec.p1.y - rec.p3.x*rec.p2.y- rec.p4.x*rec.p3.y- rec.p1.x*rec.p4.y);
         if (m>=0){
             return m;
         }else return -1*m;
     }
+
     public static double searchForP(Rectangle rec){
         return distanceBetweenPoints(rec.p1, rec.p2) + distanceBetweenPoints(rec.p2, rec.p3)+ distanceBetweenPoints(rec.p3, rec.p4) + distanceBetweenPoints(rec.p4, rec.p1);
     }
+
     public static double distanceBetweenPoints(Point p1, Point p2){
         return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
     }
 
     public static Point[] sortClockwise(Rectangle rec){
+
         Point p1 = rec.p1;
         Point p2 = rec.p2;
         Point p3 = rec.p3;
@@ -82,10 +90,10 @@ public class Rectangle {
         startList.add(p2);
         startList.add(p3);
         startList.add(p4);
-        int maxY=0;
-        int minY = 400;
-        int maxX = 0;
-        int minX = 400;
+        double maxY=0;
+        double minY = 400;
+        double maxX = 0;
+        double minX = 400;
 
         for (Point point : startList){
             if (maxY<=point.y) maxY=point.y;
@@ -93,26 +101,123 @@ public class Rectangle {
             if (maxX<=point.x) maxX=point.x;
             if (minX>=point.x) minX=point.x;
         }
+        System.out.println("maxX = "+ maxX);
+        System.out.println("minX = "+ minX);
+        System.out.println("maxY = "+ maxY);
+        System.out.println("minY = "+ minY);
 
         for (Point point : startList){
-            if (point.x==minX) answer[0] = point;
+            if (point.x==minX && ((point.y-minY)<=(maxY-point.y))) answer[0] = point;
             if (point.y==minY) answer[1] = point;
             if (point.x==maxX) answer[2] = point;
             if (point.y==maxY) answer[3] = point;
         }
         return answer;
+
     }
 
+//    public static Rectangle newCircumscribedRectangle(Rectangle startRectangle){
+//        Point[] arrayOfSortedPoints = sortClockwise(startRectangle);
+//
+//    }
     public static Rectangle createACircumscribedRectangle(Rectangle startRectangle){
+
         Point[] arrayOfSortedPoints = sortClockwise(startRectangle);
-        Point p1 = new Point(arrayOfSortedPoints[0].x,arrayOfSortedPoints[1].y);
-        Point p2 = new Point(arrayOfSortedPoints[2].x,arrayOfSortedPoints[1].y);
-        Point p3 = new Point(arrayOfSortedPoints[2].x,arrayOfSortedPoints[3].y);
-        Point p4 = new Point(arrayOfSortedPoints[0].x,arrayOfSortedPoints[3].y);
-        return new Rectangle(p1,p2,p3,p4);
+
+        System.out.println("Start of Params of firstRect "+startRectangle.p1.x+" "+startRectangle.p1.y);
+        System.out.println(startRectangle.p2.x+" "+startRectangle.p2.y);
+        System.out.println(startRectangle.p3.x+" "+startRectangle.p3.y);
+        System.out.println(startRectangle.p4.x+" "+startRectangle.p4.y+" final ");
+
+        if (startRectangle.p1.y != startRectangle.p2.y && startRectangle.p1.x != startRectangle.p2.x ) {
+
+            System.out.println(" X0= "+arrayOfSortedPoints[0].x+" Y0 ="+arrayOfSortedPoints[0].y + " X1= "+arrayOfSortedPoints[1].x+ " Y1= "+arrayOfSortedPoints[1].y  +
+                    " X2= "+arrayOfSortedPoints[2].x+"Y2 = "+arrayOfSortedPoints[2].y+" X3= "+ arrayOfSortedPoints[3].x+" Y3= "+arrayOfSortedPoints[3].y);
+
+            Point p1 = new Point(arrayOfSortedPoints[0].x, arrayOfSortedPoints[1].y);
+            Point p2 = new Point(arrayOfSortedPoints[2].x, arrayOfSortedPoints[1].y);
+            Point p3 = new Point(arrayOfSortedPoints[2].x, arrayOfSortedPoints[3].y);
+            Point p4 = new Point(arrayOfSortedPoints[0].x, arrayOfSortedPoints[3].y);
+            System.out.println("Первый вариант");
+            return new Rectangle(p1, p2, p3, p4);
+        }else {
+            Point p1 = new Point((arrayOfSortedPoints[0].x-(arrayOfSortedPoints[3].y-arrayOfSortedPoints[0].y)/2),(arrayOfSortedPoints[3].y-arrayOfSortedPoints[0].y)/2 + arrayOfSortedPoints[0].y);
+            Point p2 = new Point((arrayOfSortedPoints[1].x-arrayOfSortedPoints[0].x)/2 + arrayOfSortedPoints[0].x,arrayOfSortedPoints[1].y-(arrayOfSortedPoints[1].x-arrayOfSortedPoints[0].x)/2);
+            Point p3  = new Point(arrayOfSortedPoints[1].x+(arrayOfSortedPoints[2].y-arrayOfSortedPoints[1].y)/2,(arrayOfSortedPoints[2].y-arrayOfSortedPoints[1].y)/2 + arrayOfSortedPoints[1].y);
+            Point p4 = new Point((arrayOfSortedPoints[1].x-arrayOfSortedPoints[0].x)/2 + arrayOfSortedPoints[0].x,arrayOfSortedPoints[2].y + (arrayOfSortedPoints[2].y-arrayOfSortedPoints[1].y)/2);
+            System.out.println("Второй");
+
+            System.out.println("Sorted points:  X0= "+arrayOfSortedPoints[0].x+" Y0 ="+arrayOfSortedPoints[0].y + " X1= "+arrayOfSortedPoints[1].x+ " Y1= "+arrayOfSortedPoints[1].y  +
+                    " X2= "+arrayOfSortedPoints[2].x+"Y2 = "+arrayOfSortedPoints[2].y+" X3= "+ arrayOfSortedPoints[3].x+" Y3= "+arrayOfSortedPoints[3].y);
+            System.out.println("Points Of new Rectangle X0= "+p1.x+" Y0 ="+p1.y + " X1= "+p2.x+ " Y1= "+p2.y  +
+                    " X2= "+p3.x+"Y2 = "+p3.y+" X3= "+ p4.x+" Y3= "+p4.y);
+            return new Rectangle(p1, p2, p3, p4);
+        }
+
     }
 
 
+    public static Rectangle moving(Rectangle startRectangle,typeOfHolding type,numberOFSelected number, int k){
+        Point[] points = sortClockwise(startRectangle);
+        Rectangle rec = null;
 
+        switch (type){
+
+            case SIDE:{
+
+                switch (number){
+
+                    case FIRST:{
+                        Point p1 = new Point((points[0].x+(k-1)*(points[0].x-points[1].x)/2),(points[0].y+(k-1)*(points[0].y-points[1].y)/2));
+                        Point p2=new Point((points[1].x + (points[1].x-points[0].x)/2*(k-1)),(points[1].y + (k-1)*(points[1].y-points[0].y)/2));
+                        Point p3 = new Point((points[2].x+(k-1)*(points[2].x-points[1].x + (points[1].x-points[0].x)/2)),(points[2].y+(k-1)*(points[2].y-points[1].y +(points[1].y-points[0].y)/2 )));
+                        Point p4 = new Point((points[3].x+(k-1)*(points[3].x-points[0].x + (points[0].x-points[1].x)/2)),(points[3].y+(k-1)*(points[3].y-points[0].y+(points[0].y-points[1].y)/2)));
+
+                        System.out.println("Start of Params of firstRect "+p1.x+" "+p1.y);
+                        System.out.println(p2.x+" "+p2.y);
+                        System.out.println(p3.x+" "+p3.y);
+                        System.out.println(p4.x+" "+p4.y+" final ");
+                        startRectangle = new Rectangle(p1,p2,p3,p4);
+
+
+
+                    }
+                    case SECOND:{
+//                        Point p1 = new Point();
+//                        Point p2=new Point();
+//                        Point p3 = new Point();
+//                        Point p4 = new Point();
+//
+//                        System.out.println("Start of Params of firstRect "+p1.x+" "+p1.y);
+//                        System.out.println(p2.x+" "+p2.y);
+//                        System.out.println(p3.x+" "+p3.y);
+//                        System.out.println(p4.x+" "+p4.y+" final ");
+//                        startRectangle = new Rectangle(p1,p2,p3,p4);
+                    }
+                    case THIRD:{
+
+                    }
+                    case FORTH:{
+
+                    }
+                }
+
+            } case POINT:{
+                switch (number){
+                    case FIRST:{
+
+                    }case SECOND:{
+
+                    }case THIRD:{
+
+                    }case FORTH:{
+
+                    }
+                }
+
+            }
+        }
+    return startRectangle;
+    }
 
 }
